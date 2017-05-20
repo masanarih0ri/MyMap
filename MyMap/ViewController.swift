ba688d8b6c63c6f3630b447a977c46b07a9d6d5e
mhori <masanarihoridevelop@gmail.com>
2017-05-21 07:59:21 +0900

    [Modify]マップとテキスト表示

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Text Fieldのdelegate通知先を設定
+        inputText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var dispMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //キーボードを閉じる
+        textField.resignFirstResponder()
+        //入力された文字を取り出す
+        let searchKeyword = textField.text
+        //入力された文字をデバッグエリアに表示
+        print(searchKeyword)
+        //デフォルト操作を行うのでtrueを返す
+        return true
+    }
+    
 }
 
